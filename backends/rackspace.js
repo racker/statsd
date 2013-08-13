00f470f911a456ff83010c66b797746dd4340553
Nathan Jordan <natedagreat27274@gmail.com>
2013-08-13 13:15:05 -0700

    linted

--- backends/rackspace.js
+++ backends/rackspace.js
@@ -16,23 +16,43 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
-function RackspaceBackend(startupTime, config, stats){
+function RackspaceBackend(startupTime, config, stats) {
   var self = this;
-
   this.lastFlush = startupTime;
   this.lastException = startupTime;
   this.config = config.rackspace || {};
   this.outputDir = path.normalize(this.config.outputDir) + '/';
-  this.filename = this.outputDir + (new Date()).getTime().toString() + '.json';
-  
+  this.filename = this.outputDir + Date.now() + '.json';
   this.statsCache = {
     counters: {},
     timers: {},
     gauges: {}
   };
-
   stats.on('flush', self.flush.bind(self));
   stats.on('status', self.status.bind(self));
+}
+
+function createTimerObject(statsdTimer) {
+  return {
+    mean_90: statsdTimer.mean_90,
+    upper_90: statsdTimer.upper_90,
+    upper: statsdTimer.upper,
+    lower: statsdTimer.lower,
+    count: statsdTimer.count,
+    mean: statsdTimer.mean
+  };
+}
+
+function calculateTimerCache(cached, current) {
+  var n = cached.count;
+  return {
+    mean_90: (cached.mean_90 * n + current.mean_90) / (n + 1),
+    upper_90: Math.max(cached.upper_90, current.upper_90),
+    upper: Math.max(cached.upper, current.upper),
+    lower: Math.min(cached.lower, current.lower),
+    count: cached.count + current.count,
+    mean: (cached.mean * n + current.mean) / (n + 1)
+  };
 }
 
 /**
@@ -41,22 +61,6 @@ function RackspaceBackend(startupTime, config, stats){
   */
 RackspaceBackend.prototype.clearMetrics = function(metrics) {
   var self = this;
-/*
-  _.each(self.statsCache, function(val, type) {
-    if(!metrics[type]) {
-      return;
-    }
-
-    _.each(metrics[type], function(val, name) {
-      if(type == 'counters') {
-        self.statsCache[type][name] = 0;
-      } else if (type == 'timers') {
-        self.statsCache[type][name] = null;
-      }
-
-    });
-  });
-  */
   self.statsCache = {
     counters: {},
     timers: {},
@@ -64,73 +68,60 @@ RackspaceBackend.prototype.clearMetrics = function(metrics) {
   };
 };
 
-
 RackspaceBackend.prototype.flush = function(timestamp, metrics) {
   var self = this,
-      out;
-  //console.dir(metrics);
+    out;
   console.log('caching statsd metrics at', new Date(timestamp * 1000).toString());
-  _.each(self.statsCache, function(metric,type) {
-    
-    if(!metrics[type]) return;
-
-    _.each(metrics[type], function(metricType,name) {
+  _.each(self.statsCache, function(metric, type) {
+    if (!metrics[type]) {
+      return;
+    }
+    _.each(metrics[type], function(metricType, name) {
       var value;
-
-      if(type == 'timers')
-        if(metrics['timer_data'][name])
-          value = createTimerObject(metrics['timer_data'][name]);
-  else
-    return;
-      else if (type == 'counters') {
-        value = { rate: metrics['counter_rates'][name], i: 1.0 };
-      }
-      else if (type == 'gauges') {
+      if (type === 'timers') {
+        if (metrics.timer_data[name]) {
+          value = createTimerObject(metrics.timer_data[name]);
+        } else {
+          return;
+        }
+      } else if (type === 'counters') {
+        value = { rate: metrics.counter_rates[name], i: 1.0 };
+      } else if (type === 'gauges') {
         value = { value: metrics[type][name] };
       }
-
       if (!self.statsCache[type][name]) {
         self.statsCache[type][name] = value;
-  return;
+        return;
       }
-
-      if(type == 'counters') {
-  self.statsCache[type][name] = { 
-      rate: (self.statsCache[type][name].rate * self.statsCache[type][name].i + value.rate) / (self.statsCache[type][name].i + 1.0), 
-      i: self.statsCache[type][name].i + 1.0
-    } 
-      } else if (type == 'timers') {
-        self.statsCache[type][name] = calculateTimerCache(self.statsCache[type][name], metrics['timer_data'][name]); 
-      } else if (type == 'gauges') {
+      if (type === 'counters') {
+        self.statsCache[type][name] = { 
+          rate: (self.statsCache[type][name].rate * self.statsCache[type][name].i + value.rate) / (self.statsCache[type][name].i + 1.0), 
+          i: self.statsCache[type][name].i + 1.0
+        }; 
+      } else if (type === 'timers') {
+        self.statsCache[type][name] = calculateTimerCache(self.statsCache[type][name], metrics.timer_data[name]); 
+      } else if (type === 'gauges') {
         self.statsCache[type][name] = { value: metrics[type][name] };
       }
     });
   });
-
-  //console.dir(self.statsCache);
   if ((timestamp - this.lastFlush) < this.config.flushInterval) {
     return;
   }
-
   out = {
     counters: this.statsCache.counters,
     timers: this.statsCache.timers,
     gauges: this.statsCache.gauges,
   };
-
-  _.each( out.counters, function (counter, counters, counterName) {
-   delete counter.i;
+  _.each(out.counters, function(counter, counters, counterName) {
+    delete counter.i;
   });
-
   delete out.counters['statsd.bad_lines_seen'];
   delete out.counters['statsd.packets_received'];
-
   console.log('flushing stats to ' + filename);
-
   fs.writeFileSync(filename, JSON.stringify(out) + '\n');
   self.lastFlush = timestamp;
   self.clearMetrics(metrics);
-
 };
 
 RackspaceBackend.prototype.status = function(write) {
@@ -143,27 +134,3 @@ exports.init = function(startupTime, config, events) {
   var instance = new RackspaceBackend(startupTime, config, events);
   return true;
 };
-
-function calculateTimerCache(cached,current) {
-  var n = cached.count;
-  return {
-    mean_90: (cached.mean_90 * n + current.mean_90) / (n + 1),
-    upper_90: Math.max(cached.upper_90, current.upper_90),
-    upper: Math.max(cached.upper, current.upper),
-    lower: Math.min(cached.lower, current.lower),
-    count: cached.count + current.count,
-    mean: (cached.mean * n + current.mean) / (n + 1)
-    }
-}
-
-function createTimerObject(statsdTimer) {
-  return {
-    mean_90: statsdTimer.mean_90,
-    upper_90: statsdTimer.upper_90,
-    upper: statsdTimer.upper,
-    lower: statsdTimer.lower,
-    count: statsdTimer.count,
-    mean: statsdTimer.mean
-  }
-}
-
